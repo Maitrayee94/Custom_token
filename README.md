41b7ef3e8297b6ca04f31968469656b57c142a77
Maitrayee Das <maitrayeedas94@gmail.com>
2023-07-18 14:14:55 +0530

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Custom_token
+## Token deployed to: 0x3093C9a62340B6865a0e0A8F3a2369896Da11462
+## Exchange deployed to: 0x5Ec042Ef7bFc00e7A4439AbA3Db5c755f13E29a0
+
+Successfully verified contract Token on the block explorer.
+https://goerli.etherscan.io/address/0x3093C9a62340B6865a0e0A8F3a2369896Da11462#code
+
+Successfully verified contract Exchange on the block explorer.
+https://goerli.etherscan.io/address/0x5Ec042Ef7bFc00e7A4439AbA3Db5c755f13E29a0#code
+
+currently it will take fees as 5% of aamount and transfer to address but the conversion part from token to eth is not there, i was trying to do it by implementing interface of dex and then swap from pool, but i was run out of time.
--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,35 +2,20 @@
 pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-interface IDexPool {
-    function tokenToEthSwap(uint256 tokensToSwap, uint256 minEthToReceive) external;
-}
-
 contract Token is ERC20 {
     address public fund;
-    IDexPool public dexPool;
+    
 
     constructor() ERC20("Token", "TKN") {
         _mint(msg.sender, 1000000 * 10 ** decimals());
         fund = 0x3CDf4De397629EA4346c6Bb9ac5c29451CA0E41E; // Set the address where the tax will be transferred
     }
 
-    function setDexAddress(address _dex) external {
-        dexPool = IDexPool(_dex);
-    }
-
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
         uint256 tax = (amount * 5) / 100; // 5% tax
-        uint256 transferAmount = amount - tax;
 
-        super._transfer(sender, fund, transferAmount);
-
-        // Call the tokenToEthSwap function on the DexPool contract
-        dexPool.tokenToEthSwap(tax, 0);
-
-        
-        super._transfer(sender, recipient, tax);
+        super._transfer(sender, recipient, amount - tax);
+        super._transfer(sender, fund, tax);
 
         uint256 currentAllowance = allowance(sender, _msgSender());
         require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
